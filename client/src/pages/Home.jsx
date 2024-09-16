ad293bc48b67aceddf7835efc90ead8ff5cc44de
Rohit Raghuwanshi <rohitraghuwanshi591@gmail.com>
2024-09-16 21:38:30 +0530

    Loader

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 import CallToAction from '../components/CallToAction';
 import { useEffect, useState } from 'react';
 import PostCard from '../components/PostCard';
+import { Spinner } from 'flowbite-react';
 
 const Home = () => {
 
   const [posts, setPosts] = useState([]);
+  const [loading,setLoading] = useState(false);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -40,7 +42,7 @@ const Home = () => {
       </div>
 
       <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
-        {posts && posts.length > 0 && (
+        {posts && posts.length > 0 ? (
           <div className='flex flex-col gap-6'>
             <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
             <div className='flex flex-wrap gap-4'>
@@ -55,7 +57,7 @@ const Home = () => {
               View all posts
             </Link>
           </div>
-        )}
+        ):( <div  className='flex items-center justify-center pt-6' ><Spinner  size='xl' /></div>)}
       </div>
     </div>
   );
